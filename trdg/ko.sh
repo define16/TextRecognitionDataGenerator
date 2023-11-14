92314889c2d3bb453ef0e65d1b8c071cd80db7e9
define16 <radix7318@gmail.com>
2023-11-15 00:42:48 +0900

    dict 추가 와 스크립트 추가

--- trdg/ko.sh
+++ trdg/ko.sh
@@ -1,18 +1,18 @@
-python run.py -c 100000 -fd fonts/ko -dt dicts/ko_one.txt -k 5 -rk -na 2 -f 64 -t 8 --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/ko_one.txt -k 5 -rk -na 2 -f 96 -t 8 --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/ko_one.txt -k 5 -rk -na 2 -f 128 -t 8 --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/ko_one.txt -k 5 -rk -na 2 -f 64 -t 8 -w 10 -r -sw 0 --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/ko_one.txt -k 5 -rk -na 2 -f 96 -t 8 -w 10 -r -sw 0 --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/ko_one.txt -k 5 -rk -na 2 -f 128 -t 8 -w 10 -r -sw 0 --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/ko_dic.txt -k 5 -rk -na 2 -f 64 -t 8 --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/ko_dic.txt -k 5 -rk -na 2 -f 96 -t 8 --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/ko_dic.txt -k 5 -rk -na 2 -f 128 -t 8 --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/ko_dic.txt -k 5 -rk -na 2 -f 64 -t 8 -w 5 -r --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/ko_dic.txt -k 5 -rk -na 2 -f 96 -t 8 -w 5 -r --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/ko_dic.txt -k 5 -rk -na 2 -f 128 -t 8 -w 5 -r --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/num.txt -k 5 -rk -na 2 -f 64 -t 8 -w 10 -r -sw 0 --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/num.txt -k 5 -rk -na 2 -f 96 -t 8 -w 10 -r -sw 0 --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/num.txt -k 5 -rk -na 2 -f 128 -t 8 -w 10 -r -sw 0 --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/koAndNum_one.txt -k 5 -rk -na 2 -f 64 -t 8 -w 10 -r -sw 0 --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/koAndNum_one.txt -k 5 -rk -na 2 -f 96 -t 8 -w 10 -r -sw 0 --output_dir out/
-python run.py -c 100000 -fd fonts/ko -dt dicts/koAndNum_one.txt -k 5 -rk -na 2 -f 128 -t 8 -w 10 -r -sw 0 --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/ko_one.txt -k 5 -rk -na 0 -f 64 -t 8 --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/ko_one.txt -k 5 -rk -na 0 -f 96 -t 8 --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/ko_one.txt -k 5 -rk -na 0 -f 128 -t 8 --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/ko_one.txt -k 5 -rk -na 0 -f 64 -t 8 -w 10 -r -sw 0 --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/ko_one.txt -k 5 -rk -na 0 -f 96 -t 8 -w 10 -r -sw 0 --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/ko_one.txt -k 5 -rk -na 0 -f 128 -t 8 -w 10 -r -sw 0 --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/ko_dic.txt -k 5 -rk -na 0 -f 64 -t 8 --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/ko_dic.txt -k 5 -rk -na 0 -f 96 -t 8 --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/ko_dic.txt -k 5 -rk -na 0 -f 128 -t 8 --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/ko_dic.txt -k 5 -rk -na 0 -f 64 -t 8 -w 5 -r --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/ko_dic.txt -k 5 -rk -na 0 -f 96 -t 8 -w 5 -r --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/ko_dic.txt -k 5 -rk -na 0 -f 128 -t 8 -w 5 -r --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/num.txt -k 5 -rk -na 0 -f 64 -t 8 -w 10 -r -sw 0 --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/num.txt -k 5 -rk -na 0 -f 96 -t 8 -w 10 -r -sw 0 --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/num.txt -k 5 -rk -na 0 -f 128 -t 8 -w 10 -r -sw 0 --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/koAndNum_one.txt -k 5 -rk -na 0 -f 64 -t 8 -w 10 -r -sw 0 --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/koAndNum_one.txt -k 5 -rk -na 0 -f 96 -t 8 -w 10 -r -sw 0 --output_dir out/
+python run.py -c 100000 -fd fonts/ko -dt dicts/koAndNum_one.txt -k 5 -rk -na 0 -f 128 -t 8 -w 10 -r -sw 0 --output_dir out/
